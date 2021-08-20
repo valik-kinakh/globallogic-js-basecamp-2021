9ea979c245bb1a70bf1cd011a0c4c04c1d8df895
Valik Kinakh <valik.kinah@gmail.com>
2021-08-20 13:38:35 +0300

    task 1 fixed

--- NodeJs_Task/tutorial-1/main.js
+++ NodeJs_Task/tutorial-1/main.js
@@ -1,32 +1,40 @@
-const http=require('http');
-const port=8000;
-const testObj= {
-   first:"Valik",
-    last:"Kinakh",
-    age:17
-}
+const http = require('http');
+const port = 8000;
+const testObj = {
+  first: 'Valik',
+  last: 'Kinakh',
+  age: 18
+};
 
 
-http.createServer((req,res)=>
-{
-    res.statusCode=404;
-    //'Content-type':'text/plain'
-    // res.writeHead(res.statusCode,{'Content-type':'text/plain'});
-    // res.write("Hello NodeJs_Task");
+http.createServer((req, res) => {
+  res.statusCode = 200;
+  let contentType = 'json';
 
-    //'Content-type':'text/html'
-    // res.writeHead(res.statusCode,{'Content-type':'text/html'});
-    // res.write(`<h1>Hello NodeJs_Task</h1>`);
-
-    //'Content-type':'application/json'
-    if (res.statusCode===200) {
-        res.writeHead(res.statusCode, {'Content-type': 'application/json'});
+  if (res.statusCode === 200) {
+    switch (contentType) {
+      case  'text':
+        res.writeHead(res.statusCode, { 'Content-type': 'text/plain' });
+        res.write('Hello NodeJs_Task');
+        break;
+      case 'html':
+        res.writeHead(res.statusCode, { 'Content-type': 'text/html' });
+        res.write(`<h1>Hello NodeJs Task</h1>`);
+        break;
+      case  'json':
+        res.writeHead(res.statusCode, { 'Content-type': 'application/json' });
         res.write(JSON.stringify(testObj));
-    }else
-    {
-        res.write(`Not found`);
+        break;
+      default:
+        res.write('Not found');
+        break;
     }
-    res.end();
+  } else {
+    res.write('Not found');
+  }
+
+
+  res.end();
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
